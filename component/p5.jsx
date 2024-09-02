6635d3fe56d0f26d2a63ca306047c725e00460be
SoarinSkySagar <sagarrana.pvt@gmail.com>
2024-09-02 23:30:33 +0530

    deployment fxes

--- component/p5.jsx
+++ component/p5.jsx
@@ -1,36 +1,43 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import p5 from "p5";
 
 const P5Sketch = () => {
   const sketchRef = useRef(null);
 
   useEffect(() => {
-    const sketch = (p) => {
-      p.setup = () => {
-        p.createCanvas(window.innerWidth, window.innerHeight);
-        p.background(255);
+    let p5Instance;
+
+    const loadP5 = async () => {
+      const p5 = (await import("p5")).default;
+
+      const sketch = (p) => {
+        p.setup = () => {
+          p.createCanvas(window.innerWidth, window.innerHeight / 2);
+          p.background(255);
+        };
+
+        p.draw = () => {
+          if (p.mouseIsPressed) {
+            p.stroke(0);
+            p.strokeWeight(4);
+            p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
+          }
+        };
+
+        p.windowResized = () => {
+          p.resizeCanvas(window.innerWidth, window.innerHeight);
+          p.background(255);
+        };
       };
 
-      p.draw = () => {
-        if (p.mouseIsPressed) {
-          p.stroke(0);
-          p.strokeWeight(4);
-          p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
-        }
-      };
-
-      p.windowResized = () => {
-        p.resizeCanvas(window.innerWidth, window.innerHeight);
-        p.background(255);
-      };
+      p5Instance = new p5(sketch, sketchRef.current);
     };
 
-    const p5Instance = new p5(sketch, sketchRef.current);
+    loadP5();
 
     return () => {
-      p5Instance.remove();
+      p5Instance?.remove();
     };
   }, []);
 
